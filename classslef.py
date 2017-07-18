d5fa6d22199e406cbcdf7724f76529c375df0098
jrx <811152542@qq.com>
2017-07-18 22:46:53 +0800

    2017.7.18

---
+++ classslef.py
@@ -0,0 +1,31 @@
+#encoding=utf-8
+'''
+
+
+
+'''
+class mywife():
+    def __init__(self,age=20):
+        self.age = age
+        self.sex = 'female'
+        self.high ='165'
+        self.money = '1000000'
+        self.edu = "本科"
+    def singing(self):
+        return "后来"
+    def work(self):
+        return "扫地"
+    def program(self):
+        return "coding"
+    def __str__(self):
+        return "%s,%s,%s,%s,%s" % (self.age,self.sex,self.high,self.money,self.edu)
+    
+meinv = mywife()
+print type(mywife)
+print type(meinv)
+print meinv
+print meinv.singing()
+
+    
+        
+    
