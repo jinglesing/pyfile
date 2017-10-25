b79e39830f0bcbe9df7a850ee68ddf4d078c148c
jrx <811152542@qq.com>
2017-10-26 00:00:56 +0800

    2017.10.26

--- classslef.py
+++ classslef.py
@@ -5,7 +5,7 @@
 
 '''
 class mywife():
-    def __init__(self,age=20):
+    def __init__(self,age):
         self.age = age
         self.sex = 'female'
         self.high ='165'
@@ -20,7 +20,7 @@ class mywife():
     def __str__(self):
         return "%s,%s,%s,%s,%s" % (self.age,self.sex,self.high,self.money,self.edu)
     
-meinv = mywife()
+meinv = mywife(22)
 print type(mywife)
 print type(meinv)
 print meinv
