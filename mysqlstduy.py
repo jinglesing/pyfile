bd7a3c581ece14fbb44b8a2db41cb5761d92c52b
jrx <811152542@qq.com>
2017-10-26 22:58:32 +0800

    2017.10.26 mysql实例

--- mysqlstduy.py
+++ mysqlstduy.py
@@ -19,12 +19,51 @@ for data in cur.fetchall():
 # )
 username_jrx = 'jrx'
 passwod = '123456'
-# cur.execute("CREATE TABLE if not EXISTS userinfo3(id int PRIMARY KEY AUTO_INCREMENT,username VARCHAR(255),passwd VARCHAR(255))")
+#cur.execute("CREATE TABLE if not EXISTS userinfo3(id int PRIMARY KEY AUTO_INCREMENT,username VARCHAR(255),passwd VARCHAR(255))")
 cur.execute("insert into userinfo3(username,passwd) value('%s','%s')" %(username,passwod))#增加
 cur.execute("update userinfo3 set passwd='123456'")#改
-cur.execute("delete from userinfo3 where username='jrx'")#删
+#cur.execute("delete from userinfo3 where username='jrx'")#删
 test.commit()#数据插入，修改，删除都要commit
+
+
+#提示输入用户名密码,若用户名密码正确则提示验证成功，反之则失败
+#1.提示输入用户名密码
+#2.把用户名密码发到数据库核对
+
+#raw_input()含有参数
+username_one = raw_input(u"请输入用户名:".encode("utf-8"))
+passwd_one = raw_input(u"请输入密码:".encode("utf-8"))
+count = cur.execute("select * from userinfo3 where username='%s'and passwd='%s'"%(username_one,passwd_one))
+results = cur.fetchone()
+print "results=",results
+print "count=",count
+#count匹配数目 results None == Flase
+if results:
+    print u"验证成功"
+    print u"修改密码"
+    newpasswd = raw_input(u"请输入新密码:".encode("utf-8"))
+    print "newpaasswd=",newpasswd
+    try:
+        cur.execute("update userinfo3 set passwd='%s'where username='%s'"%(newpasswd,username_one))
+        test.commit()
+        # thispasswood=""
+        result = cur.execute("select * from userinfo3 where username='%s'"% username_one)
+        result = cur.fetchone()
+        print "result=",result
+        thispasswood = result[2]
+        print u'修改成，新密码是：%s'% thispasswood
+
+    except:
+        print u"修改出错"
+
+else:
+    print u'验证失败'
+# if count:
+#     print u"验证成功"
+# else:
+#     print u'验证失败'
+
 cur.close()
 test.close()
 
-#提示输入用户名密码
+
